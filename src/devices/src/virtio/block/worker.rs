42bb181bbea6b8f4d4a093604dbf2e019f2df45d
Jake Correnti <jakecorrenti+github@proton.me>
2024-09-03 16:31:24 -0400

    add `DiskFile` trait and implement it for `File`
    Additionally sets the disk_image type in DiskProperties
    to be Box<dyn DiskFile> instead of just File.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/devices/src/virtio/block/worker.rs
+++ src/devices/src/virtio/block/worker.rs
@@ -224,7 +224,7 @@ impl BlockWorker {
                     Err(RequestError::InvalidDataLength)
                 } else {
                     writer
-                        .write_from_at(&self.disk.file, data_len, request_header.sector * 512)
+                        .write_from_at(&*self.disk.file, data_len, request_header.sector * 512)
                         .map_err(RequestError::WritingToDescriptor)
                 }
             }
@@ -234,7 +234,7 @@ impl BlockWorker {
                     Err(RequestError::InvalidDataLength)
                 } else {
                     reader
-                        .read_to_at(&self.disk.file, data_len, request_header.sector * 512)
+                        .read_to_at(&*self.disk.file, data_len, request_header.sector * 512)
                         .map_err(RequestError::ReadingFromDescriptor)
                 }
             }
