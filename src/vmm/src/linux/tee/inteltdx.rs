5f4a8be8b833717b9fd4eacd35f480408b6e7e37
Jake Correnti <jakecorrenti+github@proton.me>
2025-02-27 16:17:51 -0500

    init memory regions for tdx
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- src/vmm/src/linux/tee/inteltdx.rs
+++ src/vmm/src/linux/tee/inteltdx.rs
@@ -11,6 +11,7 @@ pub enum Error {
     CreateTdxVmStruct,
     GetCapabilities,
     InitVm,
+    InitMemoryRegions(i32),
     OpenTdvfFirmwareFile(io::Error),
     ParseTdvfSections(tdvf::Error),
 }
@@ -51,6 +52,24 @@ impl IntelTdx {
             .init_vm(fd, cpuid)
             .or_else(|_| return Err(Error::InitVm))?;
 
+        Ok(())
+    }
+
+    pub fn configure_td_memory(&self, fd: &kvm_ioctls::VmFd, regions: &Vec<crate::vstate::MeasuredRegion>) -> Result<(), Error> {
+        for region in regions {
+            let ext = if arch::BIOS_START == region.guest_addr {
+                1
+            } else {
+                0
+            };
+
+            if let Err(e) = self.vm.init_mem_region(fd, region.guest_addr, (region.size / 4096) as u64, ext, region.host_addr) {
+                if e.code != libc::EAGAIN {
+                    return Err(Error::InitMemoryRegions(e.code));
+                }
+            }
+        }
+        
         Ok(())
     }
 }
