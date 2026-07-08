632a9d0ff2e494636a2d3c1ee414ab84c86cdc82
Jake Correnti <jakecorrenti+github@proton.me>
2026-07-08 15:10:43 -0400

    init/aws-nitro: initialize /dev/console
    Initialize /dev/console and redirect std streams for early debug output.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- init/aws-nitro/src/main.rs
+++ init/aws-nitro/src/main.rs
@@ -1,5 +1,49 @@
 use anyhow::Context;
 
+mod fs {
+    use std::fs::OpenOptions;
+    use std::os::fd::AsFd;
+
+    use anyhow::Context;
+    use nix::errno::Errno;
+    use nix::mount::{self, MsFlags};
+    use nix::unistd;
+
+    /// Initialize /dev/console and redirect std{err, in, out} to it for early debug output.
+    pub fn console_init() -> anyhow::Result<()> {
+        let path = "/dev/console";
+
+        match mount::mount(
+            Some("dev"),
+            "/dev",
+            Some("devtmpfs"),
+            MsFlags::MS_NOSUID | MsFlags::MS_NOEXEC,
+            None::<&str>,
+        ) {
+            Ok(_) => Ok(()),
+            Err(Errno::EBUSY) => Ok(()),
+            Err(e) => Err(e),
+        }?;
+
+        // Redirect stdin, stdout, and stderr to /dev/console.
+        let console_r = OpenOptions::new()
+            .read(true)
+            .open(path)
+            .context("unable to open /dev/console as read-only")?;
+
+        unistd::dup2_stdin(console_r.as_fd()).context("unable to redirect stdin")?;
+
+        let console_w = OpenOptions::new()
+            .write(true)
+            .open(path)
+            .context("unable to open /dev/console as write-only")?;
+        let console_w = console_w.as_fd();
+
+        unistd::dup2_stdout(console_w).context("unable to redirect stdout")?;
+        unistd::dup2_stderr(console_w).context("unable to redirect stderr")
+    }
+}
+
 mod kernel_mods {
     use std::fs::{self, OpenOptions};
     use std::io::{self, ErrorKind};
@@ -71,6 +115,9 @@ fn main() -> anyhow::Result<()> {
     // Some linux modules, like virtio-mmio, may be required for console output. Load these modules
     // immediately to ensure they are available to the initrd.
     kernel_mods::load_modules().context("unable to load linux kernel modules")?;
+
+    // Initialize early debug output with /dev/console.
+    fs::console_init().context("unable to initialize /dev/console")?;
 
     Ok(())
 }
