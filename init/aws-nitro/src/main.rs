2f0a392bcd75f3ea7127aeeb0e545d1a93a69b38
Jake Correnti <jakecorrenti+github@proton.me>
2026-07-13 08:55:02 -0400

    init/aws-nitro: extract rootfs
    Extract the rootfs tarball from the memory buffer that read the rootfs
    archive from the hypervisor, and write it to the enclave's filesystem.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- init/aws-nitro/src/main.rs
+++ init/aws-nitro/src/main.rs
@@ -272,6 +272,8 @@ mod nsm {
     use aws_nitro_enclaves_nsm_api::api::{Request, Response};
     use aws_nitro_enclaves_nsm_api::driver as nitro_driver;
 
+    const NSM_PCR_CHUNK_SIZE: usize = 0x800; // 2KiB
+    const NSM_PCR_ROOTFS: u16 = 16;
     const NSM_PCR_EXEC_DATA: u16 = 17;
 
     /// Measure the enclave execution environment {path, argv, envp} in NSM PCR 17.
@@ -312,6 +314,67 @@ mod nsm {
             r => bail!("unexpected NSM response: {:?}", r),
         }
     }
+
+    /// Extend the rootfs NSM PCR with a data block from the TAR archive
+    pub fn pcr_extend_rootfs(nsm_fd: i32, rootfs: &[u8]) -> anyhow::Result<()> {
+        // Measure the root filesystem with NSM PCR 16. NSM PCR extension requests have a data size
+        // cap of 4KiB (usually smaller than the rootfs size). Therefore, measure the rootfs in
+        // 2KiB chunks.
+        for chunk in rootfs.chunks(NSM_PCR_CHUNK_SIZE) {
+            let req = Request::ExtendPCR {
+                index: NSM_PCR_ROOTFS,
+                data: chunk.to_vec(),
+            };
+            let resp = nitro_driver::nsm_process_request(nsm_fd, req);
+            match resp {
+                Response::ExtendPCR { .. } => continue,
+                Response::Error(e) => bail!("failure to extend PCR {}: {:?}", NSM_PCR_ROOTFS, e),
+                r => bail!("unexpected NSM response: {:?}", r),
+            }
+        }
+        Ok(())
+    }
+}
+
+mod archive {
+    use std::io::{Cursor, Read};
+
+    use anyhow::Context;
+
+    /// Extract the tarball from the reader (that is, the memory buffer that
+    /// read the rootfs archive from the hypervisor vsock) and write it to the
+    /// enclave's filesystem.
+    pub fn extract(nsm_fd: i32, rootfs_archive: &[u8]) -> anyhow::Result<()> {
+        // Measure the rootfs data in NSM PCR 16 before extraction.
+        let mut tar = tar::Archive::new(Cursor::new(rootfs_archive));
+        for entry in tar.entries().context("unable to read tar entries")? {
+            let mut entry = entry.context("unable to read tar entry")?;
+            let path = entry.path().context("unable to read entry path")?;
+
+            let ignored_paths = ["rootfs/etc/hostname", "rootfs/etc/hosts"];
+            if ignored_paths
+                .iter()
+                .any(|p| path.to_string_lossy().contains(p))
+            {
+                continue;
+            }
+
+            let mut data = Vec::new();
+            entry
+                .read_to_end(&mut data)
+                .context("unable to read entry data")?;
+            if !data.is_empty() {
+                super::nsm::pcr_extend_rootfs(nsm_fd, &data)
+                    .context("unable to extend pcr with rootfs")?;
+            }
+        }
+
+        // Extract the archive to the root filesystem.
+        let mut tar = tar::Archive::new(Cursor::new(rootfs_archive));
+        tar.set_preserve_permissions(true);
+        tar.set_preserve_ownerships(true);
+        tar.unpack("/").context("unable to extract rootfs archive")
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -339,6 +402,9 @@ fn main() -> anyhow::Result<()> {
 
     // Measure the rootfs and execution environment in the NSM PCRs.
     nsm::pcr_extend_exec_path(nsm_fd, &args.exec_path, &args.exec_argv, &args.exec_envp)?;
+
+    // Extract the rootfs from memory and write it to the enclave filesystem.
+    archive::extract(nsm_fd, &args.rootfs_archive)?;
 
     Ok(())
 }
