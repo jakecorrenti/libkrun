aeaad0628fa983267530624b4cb85dd50e8ee8fa
Jake Correnti <jakecorrenti+github@proton.me>
2026-07-16 15:50:16 -0400

    init/aws-nitro: parent process shoudld wait for enclave app to exit
    The parent process should wait for the enclave's application to exit.
    Once the child process has exited, shutdown the proxies and write the
    return code to the host.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- init/aws-nitro/src/main.rs
+++ init/aws-nitro/src/main.rs
@@ -1,11 +1,19 @@
 use std::ffi::CString;
-use std::str::FromStr;
+use std::io::{Read, Write};
+use std::os::fd::OwnedFd;
+use std::time::Duration;
 
 use anyhow::{Context, bail};
 use nix::libc as nix_c;
+use nix::sys::signal;
+use nix::sys::wait::{self, WaitStatus};
 use nix::unistd::{self, ForkResult};
+use signal_hook::consts::SIGTERM;
+use signal_hook::iterator::Signals;
+use vsock::{VMADDR_CID_HOST, VsockAddr, VsockStream};
 
 const VSOCK_PORT_OFFSET_ARGS_READER: u32 = 1;
+const VSOCK_PORT_OFFSET_APP_RET_CODE: u32 = 4;
 
 mod fs {
     use std::fs::OpenOptions;
@@ -628,6 +636,8 @@ mod proxy {
     fn init_output_proxy(vsock_port: u32) -> anyhow::Result<()> {
         let addr = VsockAddr::new(VMADDR_CID_HOST, vsock_port);
         let stream = VsockStream::connect(&addr).context("unable to connect to host vsock")?;
+        // STDERR and STDOUT now point to the vsock, so we can let the original vsock fd be dropped
+        // when it goes out of scope and close the streams when we're ready.
         unistd::dup2_stderr(&stream).context("unable to redirect stderr to vsock")?;
         unistd::dup2_stdout(&stream).context("unable to redirect stdout to vsock")
     }
@@ -1091,10 +1101,13 @@ mod proxy {
         Ok(())
     }
 
-    pub fn init(cid: u32, args: &super::args_reader::EnclaveArgs) -> anyhow::Result<()> {
+    pub fn init(
+        cid: u32,
+        args: &super::args_reader::EnclaveArgs,
+        shutdown_read: &OwnedFd,
+        shutdown_write: &OwnedFd,
+    ) -> anyhow::Result<()> {
         let (readp, writep) = nix::unistd::pipe().context("unable to create readiness pipe")?;
-        let (shutdown_read, shutdown_write) =
-            nix::unistd::pipe().context("unable to create shutdown pipe")?;
 
         // If not running in debug mode, initialize the application output proxy.
         // Otherwise, the enclave uses the console (which is already connected)
@@ -1108,8 +1121,8 @@ mod proxy {
             init_network_proxy(
                 &readp,
                 &writep,
-                &shutdown_write,
-                &shutdown_read,
+                shutdown_write,
+                shutdown_read,
                 cid + VSOCK_PORT_OFFSET_NET,
             )?;
         }
@@ -1118,8 +1131,8 @@ mod proxy {
         init_signal_handler_proxy(
             &readp,
             &writep,
-            &shutdown_write,
-            &shutdown_read,
+            shutdown_write,
+            shutdown_read,
             cid + VSOCK_PORT_OFFSET_SIGNAL_HANDLER,
         )?;
 
@@ -1150,6 +1163,54 @@ fn launch(argv: Vec<String>, envp: Vec<String>) -> anyhow::Result<()> {
     }
 
     unistd::execvpe(&argv_cstr[0], &argv_cstr, &envp_cstr).context("unable to call execvpe")?;
+
+    Ok(())
+}
+
+/// Dereference and close the application output vsock.
+fn close_app_output() -> anyhow::Result<()> {
+    unistd::close(nix_c::STDOUT_FILENO).context("unable to close STDOUT fd")?;
+    unistd::close(nix_c::STDERR_FILENO).context("unable to close STDERR fd")?;
+    Ok(())
+}
+
+/// CLose and exit each device proxy.
+fn exit_proxies(output_enabled: bool, shutdown_write: &OwnedFd) -> anyhow::Result<()> {
+    // The shutdown value is irrelevant, it acts as a signal to all device proxy
+    // threads that the enclave is exiting. Upon receiving this signal, each
+    // device proxy will close their respective vsock and exit.
+    unistd::write(shutdown_write, &[1]).context("unable to write to shutdown pipe")?;
+
+    // If not in debug mode, close the application output vsock.
+    if output_enabled {
+        close_app_output()?;
+    }
+
+    Ok(())
+}
+
+/// Forward the application return code to the host.
+fn write_app_ret(code: i32, cid: u32) -> anyhow::Result<()> {
+    let vsock_port = cid + VSOCK_PORT_OFFSET_APP_RET_CODE;
+    let addr = VsockAddr::new(VMADDR_CID_HOST, vsock_port);
+    let mut stream = VsockStream::connect(&addr).context("unable to connect to host")?;
+
+    // The host needs to join all device proxy threads before reading the return code. Allow some
+    // time for the host to connect to the return code vsock.
+    stream.set_write_timeout(Some(Duration::from_secs(5)))?;
+    stream.set_read_timeout(Some(Duration::from_secs(5)))?;
+
+    // Write the return code.
+    stream
+        .write_all(&code.to_ne_bytes())
+        .context("unable to write return code to host")?;
+
+    // Read a return code (value is irrelevant) from the host. This is to ensure that the host was
+    // able to read the return code from the vsock before the enclave exits.
+    let mut read_code_buf = [0u8; 4];
+    stream
+        .read_exact(&mut read_code_buf)
+        .context("unable to read return code confirmation from host")?;
 
     Ok(())
 }
@@ -1195,15 +1256,45 @@ fn main() -> anyhow::Result<()> {
     // Initialize the cgroups.
     fs::init_cgroups()?;
 
+    let (shutdown_readp, shutdown_writep) = unistd::pipe()?;
+
     // Initialize each configured device proxy.
-    proxy::init(cid, &args)?;
+    proxy::init(cid, &args, &shutdown_readp, &shutdown_writep)?;
 
     match unsafe { unistd::fork()? } {
         ForkResult::Parent { child } => {
             // Initialize the shutdown handler for signals to be forwarded to the application
             // process.
+            let mut signals = Signals::new([SIGTERM])?;
+            std::thread::spawn(move || {
+                for signal in signals.forever() {
+                    if signal == SIGTERM {
+                        // Send the signal to the application process.
+                        let _ = signal::kill(child, signal::SIGTERM);
+                    }
+                }
+            });
+
+            // Wait for the application process to exit.
+            let code = match wait::waitpid(child, None)? {
+                // If the process was ended by a signal, the return code may
+                // represent a value that under normal circumstances would
+                // indicate an error. Therefore, if the application ended from
+                // a signal, zero-out the return code (indicating that the
+                // application process exited gracefully).
+                WaitStatus::Exited(_, code) => code,
+                _ => 0,
+            };
+            // Close and exit each device proxy.
+            exit_proxies(args.app_output, &shutdown_writep)?;
+            // Write the return code to the host.
+            write_app_ret(code, cid)?;
         }
         ForkResult::Child => {
+            // Drop the shutdown pipe's so that we don't leak them into the exec'd application.
+            drop(shutdown_writep);
+            drop(shutdown_readp);
+
             // Execute the enclave application.
             launch(args.exec_argv, args.exec_envp)?;
         }
